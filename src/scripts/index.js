dd1d8827da972834b2b1cb62135fbfc45592af19
DogeOfFreedom <jchensi2000@gmail.com>
2024-02-14 21:43:57 +1100

    add basic html and css for bank form

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,8 @@
 import "../style.css";
+
+const form = document.querySelector("form");
+const submit = document.querySelector("button");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
